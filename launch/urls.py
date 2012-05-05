2acfbbf1fb02556969444714d31e5b9f808c750c
Michał Adamski <michal@ert.pl>
2012-05-05 15:51:00 +0200

    urls bugs

--- launch/urls.py
+++ launch/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('launch.views',
-    url(r'', 'index_view', name='launch_index'),
+    url(r'^$', 'index_view', name='launch_index'),
 )
