8ec8a94b0b22925c4a911de85f964e090f06bc50
Michał Adamski <michal@ert.pl>
2012-05-05 23:10:52 +0200

    skills from linked in are printable in a profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from .models import Skill
 
+
+@login_required
 def profile_view(request, template_name='accounts/profile.html'):
-    context = {}
+    skill_list = request.user.get_profile().skills_offered.all()
+    context = {
+        'skill_list': skill_list,
+    }
     return render_to_response(template_name, context, RequestContext(request))
