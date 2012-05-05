26f8a28ad39b395dab65d0b30435a997d42f348b
Michał Adamski <michal@ert.pl>
2012-05-06 00:18:01 +0200

    cleanup

--- swb12/urls.py
+++ swb12/urls.py
@@ -7,12 +7,12 @@ from django.views.generic.simple import direct_to_template
 
 #Static Pages in main app
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {"template": "../templates/index.html"}, name="home"),
-    url(r"^about$", direct_to_template, {"template": "../../main/templates/about.html"}, name="about"),
-    url(r"^terms/$", direct_to_template, {"template": "../../main/templates/terms.html"}, name="terms"),
-    url(r"^privacy/$", direct_to_template, {"template": "../../main/templates/privacy.html"}, name="privacy"),
-    url(r"^dmca/$", direct_to_template, {"template": "../../main/templates/dmca.html"}, name="dmca"),
-    url(r"^what_next/$", direct_to_template, {"template": "../../main/templates/what_next.html"}, name="what_next"),
+    url(r"^$", direct_to_template, {"template": "index.html"}, name="home"),
+    url(r"^about$", direct_to_template, {"template": "about.html"}, name="about"),
+    url(r"^terms/$", direct_to_template, {"template": "terms.html"}, name="terms"),
+    url(r"^privacy/$", direct_to_template, {"template": "privacy.html"}, name="privacy"),
+    url(r"^dmca/$", direct_to_template, {"template": "dmca.html"}, name="dmca"),
+    url(r"^what_next/$", direct_to_template, {"template": "what_next.html"}, name="what_next"),
 )
 
 #apps
