dcdf44b7ba1c6f17670d3e71f8b572c6090912f4
Michał Adamski <michal@ert.pl>
2012-05-05 21:34:00 +0200

    more readable code

--- listing/views.py
+++ listing/views.py
@@ -1,13 +1,10 @@
 def submit_request(request):
-    if request.method == 'POST':
-        form = SubmitRequest(request.POST)
-        if form.is_valid():
-            print form
-            print dir(form)
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
-    else:
-        form = SubmitRequest() 
-        
+    form = SubmitRequest(request.POST or None)
+    if form.is_valid():
+        print form
+        print dir(form)
+        return HttpResponseRedirect('/thanks/') # Redirect after POST
+
     return render_to_response('contact.html', {
         'form': form,
     })
