2f25164de4107750c69f3077fdfc9e4ec26542f8
Tim Weiss <timweiss1981@googlemail.com>
2012-05-05 22:16:02 +0200

    listing

--- listing/views.py
+++ listing/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+from listings.models import Problem
+
 def submit_request(request):
     form = SubmitRequest(request.POST or None)
     if form.is_valid():
@@ -8,3 +11,10 @@ def submit_request(request):
     return render_to_response('contact.html', {
         'form': form,
     })
+
+
+def listings(request):
+    problems = Problem.objects.all()
+    return render(request, 'listing.html', {
+        'problems': problems,
+        })
