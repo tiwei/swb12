5dd8d1f039a35270f9f1b9fa48a75cf809610a07
Michał Adamski <michal@ert.pl>
2012-05-06 13:12:51 +0200

    countries and cities editable in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,11 @@
 from .models import Skill, UserProfile, City, Country
 from django.contrib import admin
- 
- 
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+
+
 admin.site.register(Skill)
 admin.site.register(UserProfile)
 admin.site.register(City)
-admin.site.register(Country)
+admin.site.register(Country, CountryAdmin)
