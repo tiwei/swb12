6036e7f31deec2ff0c0933c31c17874c32e42251
ahm@berlin.com <a7madm3alla@hotmail.com>
2012-05-06 15:49:06 +0200

    listing persons and problems working

--- listing/views.py
+++ listing/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .forms import SubmitRequest
 from .models import ProblemForm
 from .models import Problem
+from django.contrib.auth.models import User
+from accounts.models import UserProfile
 
-def submit_request(request):
+def submit_request(request): #we dont use this view anywhere, if you wrote it, please delete or comment where we use it
     form = SubmitRequest(request.POST or None)
     if form.is_valid():
         print form
@@ -16,15 +18,24 @@ def submit_request(request):
 
 def listings(request):
     problems = Problem.objects.all()
+    problems_count = Problem.objects.count()
+    persons_count = UserProfile.objects.count()
+
     return render(request, 'listing_problems.html', {
         'problems': problems,
+        'persons_count':persons_count,
+        'problems_count': problems_count,
         })
 
 
 def listings_persons(request):
-    persons = Person.objects.all()
+    persons = UserProfile.objects.all()
+    persons_count = UserProfile.objects.count()
+    problems_count = Problem.objects.count()
     return render(request, 'listing_persons.html', {
         'persons': persons,
+        'persons_count':persons_count,
+        'problems_count': problems_count,
         })
     
 def submit_problem(request):
